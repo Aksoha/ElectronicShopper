e39db9bb66237cf406707d62c260c34d58f7f014
Łukasz Suliga <grandstais3@gmail.com>
2022-10-16 13:25:21 +0200

    Add Category Update Stored Procedure

---
+++ ElectronicShopperData/dbo/Stored Procedures/Category/spCategory_Update.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spCategory_Update]
+	@Id INT,
+	@ParentId INT,
+	@CategoryName NVARCHAR(100)
+
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	UPDATE Category
+	SET ParentId = @ParentId, CategoryName = @CategoryName
+	WHERE Id = @Id
+
+END
