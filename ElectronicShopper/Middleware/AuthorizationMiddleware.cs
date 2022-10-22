caf8c4dd5f203ba67c1b49aaf706950167aef95a
Łukasz Suliga <grandstais3@gmail.com>
2022-10-22 10:42:49 +0200

    feat: add validation of user credentials to login page
    Moves a responsibility of data validation from AuthorizationMiddleware to login page.

--- ElectronicShopper/Middleware/AuthorizationMiddleware.cs
+++ ElectronicShopper/Middleware/AuthorizationMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Security.Authentication;
 using ElectronicShopper.DataAccess.Identity;
 using ElectronicShopper.Library;
 using ElectronicShopper.Library.Models;
@@ -12,18 +13,20 @@ public class AuthorizationMiddleware
         = new ConcurrentDictionary<Guid, LoginModel>();
 
     private readonly RequestDelegate _next;
+    private readonly ILogger<AuthorizationMiddleware> _logger;
 
     private const string LoginUrl = "/login";
     private const string LogoutUrl = "/logout";
-    
+
     /// <summary>
     /// Name of a key that has to be passed in the <see cref="HttpContext"/> in order to intercept a call.
     /// </summary>
     private const string KeyName = "key";
 
-    public AuthorizationMiddleware(RequestDelegate next)
+    public AuthorizationMiddleware(RequestDelegate next, ILogger<AuthorizationMiddleware> logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     /// <summary>
@@ -47,7 +50,7 @@ public class AuthorizationMiddleware
     {
         if (context.Request.Path == LoginUrl && context.Request.Query.ContainsKey(KeyName))
         {
-            await Login(context, signInManager, userManager);
+            await Login(context, signInManager);
         }
         else if (context.Request.Path == LogoutUrl)
         {
@@ -59,29 +62,19 @@ public class AuthorizationMiddleware
         }
     }
 
-    private static async Task Login(HttpContext context, SignInManager<ApplicationUser> signInManager,
-        UserManager<ApplicationUser> userManager)
+    private async Task Login(HttpContext context, SignInManager<ApplicationUser> signInManager)
     {
         var key = Guid.Parse(context.Request.Query[KeyName]);
         var info = Logins[key];
-
-        var user = await userManager.FindByEmailAsync(info.Email);
-        if (user is null)
-        {
-            RedirectToLoginIncorrectCredentials(context);
-            return;
-        }
-
-        var validCredentials = await signInManager.UserManager.CheckPasswordAsync(user, info.Password);
-        if (validCredentials == false)
-        {
-            RedirectToLoginIncorrectCredentials(context);
-            return;
-        }
-
+        
         var result =
             await signInManager.PasswordSignInAsync(info.Email, info.Password, info.RememberMe, lockoutOnFailure: true);
 
+        if (result == SignInResult.Failed)
+        {
+            _logger.LogError(new AuthenticationException(), "Authentication has failed");
+        }
+        
         if (result.Succeeded)
         {
             Logins.Remove(key);
@@ -89,12 +82,14 @@ public class AuthorizationMiddleware
         }
         else if (result.RequiresTwoFactor)
         {
-            throw new NotImplementedException();
+            var ex = new NotImplementedException();
+            _logger.LogError(ex, "Attempted to use unsupported feature");
+            throw ex;
         }
         else if (result.IsLockedOut)
         {
-            const string errorMessage = "Your account has been blocked";
-            context.Response.Redirect($"{LoginUrl}?errorMessage={errorMessage}");
+            _logger.LogError(new AuthenticationException(), "Provided incorrect user credentials");
+            context.Response.Redirect("/");
         }
         else
         {
@@ -107,10 +102,5 @@ public class AuthorizationMiddleware
         await signInManager.SignOutAsync();
         context.Response.Redirect("/");
     }
-
-    private static void RedirectToLoginIncorrectCredentials(HttpContext context)
-    {
-        const string errorMessage = "Incorrect email and/or password";
-        context.Response.Redirect($"{LoginUrl}?errorMessage={errorMessage}");
-    }
+    
 }
