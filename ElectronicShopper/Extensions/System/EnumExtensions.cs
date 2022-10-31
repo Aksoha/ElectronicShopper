0e237d71adaaf73ae27db83ef6128b278eccf8fe
Łukasz Suliga <grandstais3@gmail.com>
2022-10-31 09:40:38 +0100

    feat(category): add category page

---
+++ ElectronicShopper/Extensions/System/EnumExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace ElectronicShopper.Extensions.System;
+
+public static class EnumExtensions
+{
+    public static string? GetEnumDisplayName(this Enum e)
+    {
+        return e.GetType().GetMember(e.ToString()).First().GetCustomAttribute<DisplayAttribute>()?.Name;
+    }
+}
