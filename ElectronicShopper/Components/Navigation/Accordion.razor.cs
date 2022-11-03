6099533d3bfe598ab5be154ecb41b5e4eed34137
Łukasz Suliga <grandstais3@gmail.com>
2022-11-03 11:08:39 +0100

    feat(accordion): add component

---
+++ ElectronicShopper/Components/Navigation/Accordion.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ElectronicShopper.Components.Navigation;
+
+public partial class Accordion : ComponentBase
+{
+#nullable disable
+    [Parameter] public RenderFragment ChildContent { get; set; }
+
+    [Parameter] public string CssClass { get; set; } = "accordion";
+#nullable enable
+}
---
+++ ElectronicShopper/Components/Navigation/AccordionItem.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ElectronicShopper.Components.Navigation;
+
+public partial class AccordionItem : ComponentBase
+{
+#nullable disable
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    [Parameter]
+    public bool Collapsed { get; set; }
+
+    [Parameter]
+    public string Title { get; set; } = string.Empty;
+
+    [Parameter]
+    public string HeaderCssClass { get; set; } = "accordion-header";
+
+    [Parameter]
+    public string ContentCssClass { get; set; } = "accordion-body";
+#nullable disable
+    private void Toggle()
+    {
+        Collapsed = !Collapsed;
+    }
+}
